b696b4fe6c4a654dc14305c5e1584623e1b2c54e
ouvreboite <muscatjb@gmail.com>
2020-05-14 16:57:23 +0200

    Setup consumers
    Setup KDSL script
    Setup faust processor
    Setup consumer of http server

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -28,31 +28,39 @@ class TransformedStation(faust.Record):
     order: int
     line: str
 
-
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+topic = app.topic("udacity.datastream.1.connect.stations", value_type=Station)
+out_topic = app.topic("org.chicago.cta.stations.table.v1", partitions=1, value_type=TransformedStation)
+table = app.Table(
+    "transformedStation",
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
+@app.agent(topic)
+async def transform_stations(stations):
+    async for station in stations:
+        line = get_line(station)
+        if line is None:
+            continue
+        transformed = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=line
+        )
+        table[station.station_id] = transformed
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
-
+def get_line(station):
+    if station.red:
+        return "red"
+    elif station.blue:
+        return "blue"
+    elif station.green:
+        return "green"
+    else:
+        return None
 
 if __name__ == "__main__":
     app.main()
